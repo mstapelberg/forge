cb75f2e3f4589872aea1ce44316ea1dc24012cb2
Myles Stapelberg <myless@mit.edu>
2025-06-23 10:32:53 -0700

    updated data.py

--- forge/workflows/allegro_utils/data.py
+++ forge/workflows/allegro_utils/data.py
@@ -46,5 +46,5 @@ class CustomSamplingASEDataModule(ASEDataModule):
             sampler=sampler,
             batch_sampler=None, # Sampler and batch_sampler are mutually exclusive
             shuffle=False, # Shuffle is mutually exclusive with a sampler
-            collate_fn=self.collate_fn, # Use the collate_fn from the parent class
+            collate_fn=self.train_dataset.collate_fn, # The collate_fn lives on the *dataset*
         ) 
